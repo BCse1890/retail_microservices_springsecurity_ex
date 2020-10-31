70f1914c540f5abd445969c684fa669f8b034181
BCse1890 <bob.currie@fsresearch.co.uk>
2020-10-31 11:39:27 +0000

    Small refactor

--- productservice/src/main/java/com/bc/springcloud/controller/ProductController.java
+++ productservice/src/main/java/com/bc/springcloud/controller/ProductController.java
@@ -2,13 +2,13 @@ package com.bc.springcloud.controller;
 
 import com.bc.springcloud.dto.Coupon;
 import com.bc.springcloud.model.Product;
+import com.bc.springcloud.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.bc.springcloud.repository.ProductRepository;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -25,10 +25,11 @@ public class ProductController {
     @Value("${couponService.url}")
     private String CouponServiceURL;
 
-    @RequestMapping(value="/products", method= RequestMethod.POST)
+    @PostMapping("/products")
     public Product createProduct(@RequestBody Product product) {
         Coupon coupon = restTemplate.getForObject(CouponServiceURL + product.getCouponCode(), Coupon.class);
         product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
         return productRepository.save(product);
     }
+
 }
